6d92456e0a5fa6e9665f592f78642e6ebfc077e6
a-yasinsky <anatoly@yasinsky.pro>
2021-07-07 02:15:18 +0600

    click interface added

--- flight_optimizer/cli.py
+++ flight_optimizer/cli.py
@@ -1,2 +1,15 @@
-def cli():
-    print("Hello World")
+import click
+
+@click.command()
+@click.option("--from", "city_from", required=True,
+    help="Departure city",
+)
+@click.option("--to", "city_to", required=True,
+    help="List of potential destination cities",
+)
+@click.argument('cities_to', nargs=-1)
+def cli(city_from, city_to, cities_to):
+    print(city_from + " - " + city_to + " " + ",".join(cities_to))
+
+if __name__ == '__main__':
+    cli()
